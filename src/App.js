02b46879ddee36089432999c417117ba86e1efef
Jake Walsh <jkwalsh127@gmail.com>
2022-09-09 20:00:50 -0700

    import radio buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import MetaMask from "./components/MetaMask";
+import { useState } from "react";
+import MetaMask from "./components/Methods/MetaMask";
+import RadioButtons from "./components/RadioButtons";
 
 
 function App() {
+  const [connectionMethod, setConnectionMethod] = useState("");
+  const handleConnectionMethodChange = (selection) => setConnectionMethod(selection);
 
   return (
     <div className="app">
       <div className="content-container">
+        <RadioButtons handleConnectionMethodChange={handleConnectionMethodChange}/>
         <MetaMask />
       </div>
     </div>
